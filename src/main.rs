c4ba9a79f2342fbf5bf7cd9df0a293c1eb1bf2d4
Tx0sh1 <clydemojura@protonmail.com>
2025-04-24 19:03:13 +0200

    basic calculator

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+fn main() {
+    let mut function = String::new();
+    println!("Enter the function name (add, subtract, multiply, divide or exit): ");
+    std::io::stdin()
+        .read_line(&mut function)
+        .expect("Failed to read line");
+    let function = function.trim();
+
+    if function == "add" {
+        add();
+    } else if function == "subtract" {
+        subtract();
+    } else if function == "multiply" {
+        multiply();
+    } else if function == "divide" {
+        divide();
+    } else if function == "exit" {
+        println!("Exiting...");
+        return;
+    }
+    else {
+        println!("Function not found");
+    }
+}
+
+fn add() {
+
+    let a: i32 = String::from("Enter first number: ")
+        .parse()
+        .expect("Failed to parse input");
+    let b: i32 = String::from("Enter second number: ")
+        .parse()
+        .expect("Failed to parse input");
+
+    let sum = a + b;
+    println!("The sum is: {}", sum);
+}
+
+fn subtract() {
+
+    let a: i32 = String::from("Enter first number: ")
+        .parse()
+        .expect("Failed to parse input");
+    let b: i32 = String::from("Enter second number: ")
+        .parse()
+        .expect("Failed to parse input");
+
+    let difference = a - b;
+    println!("The difference is: {}", difference);
+}
+
+fn multiply() {
+
+    let a: i32 = String::from("Enter first number: ")
+        .parse()
+        .expect("Failed to parse input");
+    let b: i32 = String::from("Enter second number: ")
+        .parse()
+        .expect("Failed to parse input");
+
+    let product = a * b;
+    println!("The product is: {}", product);
+}
+
+fn divide() {
+
+    let a: i32 = String::from("Enter first number: ")
+        .parse()
+        .expect("Failed to parse input");
+    let b: i32 = String::from("Enter second number: ")
+        .parse()
+        .expect("Failed to parse input");
+
+    if b != 0 {
+        let quotient = a / b;
+        println!("The quotient is: {}", quotient);
+    } else {
+        println!("Cannot divide by zero");
+    }
+}
